c0924649378793b8842c1b0f78eaa53a41c5bc7f
dqf <897432098@qq.com>
2017-09-21 12:20:10 +0800

    修改部分功能实现

---
+++ QFTableProject/QFTool/Cells/QFCustomCell.h
@@ -0,0 +1,14 @@
+//
+//  QFCustomCell.h
+//  QFTableProject
+//
+//  Created by dqf on 2017/9/21.
+//  Copyright © 2017年 dqfStudio. All rights reserved.
+//
+
+#import "QFBaseCell.h"
+
+@interface QFCustomCell : QFBaseCell
+@property (nonatomic) CGRect   m_frame;
+@property (nonatomic) UIColor  *m_backgroundColor;
+@end
