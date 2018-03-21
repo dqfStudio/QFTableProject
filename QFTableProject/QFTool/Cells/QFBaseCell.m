5cf7e2167e1554dab3b1efb5bd974ed5ed9e2228
dqf <897432098@qq.com>
2018-03-21 10:01:32 +0800

    优化部分代码

--- QFTableProject/QFTool/Cells/QFBaseCell.m
+++ QFTableProject/QFTool/Cells/QFBaseCell.m
@@ -11,22 +11,39 @@
 @implementation QFBaseCell
 
 + (id)registerTable:(UITableView *)table {
-    return [self registerTable:table initBlock:nil];
+    return [self registerTable:table style:UITableViewCellStyleDefault reuseIdentifier:NSStringFromClass(self.class) initBlock:nil];
 }
 
-+ (id)registerTable:(UITableView *)table initBlock:(QFCellInitBlock)block {
-    return [self registerTable:table style:UITableViewCellStyleDefault initBlock:block];
-}
+
 
 + (id)registerTable:(UITableView *)table style:(UITableViewCellStyle)style {
-    return [self registerTable:table style:style initBlock:nil];
+    return [self registerTable:table style:style reuseIdentifier:NSStringFromClass(self.class) initBlock:nil];
+}
++ (id)registerTable:(UITableView *)table reuseIdentifier:(NSString *)reuseIdentifier {
+    return [self registerTable:table style:UITableViewCellStyleDefault reuseIdentifier:reuseIdentifier initBlock:nil];
+}
++ (id)registerTable:(UITableView *)table initBlock:(QFCellInitBlock)block {
+    return [self registerTable:table style:UITableViewCellStyleDefault reuseIdentifier:NSStringFromClass(self.class) initBlock:block];
 }
 
+
+
 + (id)registerTable:(UITableView *)table style:(UITableViewCellStyle)style initBlock:(QFCellInitBlock)block {
-    UITableViewCell *cell = [table dequeueReusableCellWithIdentifier:NSStringFromClass(self.class)];
+    return [self registerTable:table style:style reuseIdentifier:NSStringFromClass(self.class) initBlock:block];
+}
++ (id)registerTable:(UITableView *)table reuseIdentifier:(NSString *)reuseIdentifier initBlock:(QFCellInitBlock)block {
+    return [self registerTable:table style:UITableViewCellStyleDefault reuseIdentifier:reuseIdentifier initBlock:block];
+}
++ (id)registerTable:(UITableView *)table style:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    return [self registerTable:table style:style reuseIdentifier:reuseIdentifier initBlock:nil];
+}
+
+
+
++ (id)registerTable:(UITableView *)table style:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier initBlock:(QFCellInitBlock)block {
+    UITableViewCell *cell = [table dequeueReusableCellWithIdentifier:reuseIdentifier];
     if (!cell) {
-        cell = [[self alloc] initWithStyle:style reuseIdentifier:NSStringFromClass(self.class)];
-        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell = [[self alloc] initWithStyle:style reuseIdentifier:reuseIdentifier];
         if (block) {
             block(cell);
         }
